829774d0edcf4e5755be385994201198b246affd
김윤경 <yun@gim-yungyeong-ui-MacBookAir.local>
2023-05-18 12:48:33 +0900

    api 컴포넌트 분리

---
+++ coz-shopping/src/api/ItemFetcher.js
@@ -0,0 +1,24 @@
+import  { useState, useEffect } from "react";
+
+const ItemFetcher = ({ render }) => {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    try {
+      const response = await fetch('http://cozshopping.codestates-seb.link/api/v1/products');
+      const data = await response.json();
+      setItems(data);
+    } catch (error) {
+      console.error('Error fetching items:', error);
+    }
+  };
+
+  return render(items);
+};
+
+export default ItemFetcher;
